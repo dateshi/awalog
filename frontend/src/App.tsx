a2791837a92060dd9354146dfe125e35265ea2bb
Kota-iwadate <beeguru122@gmail.com>
2022-06-16 00:07:33 +0900

    test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn AWA Log
         </a>
       </header>
     </div>
