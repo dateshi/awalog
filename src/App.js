6a76f1802c7bb826a1f18ef07fa7fd4be04c30ad
Kota-iwadate <beeguru122@gmail.com>
2022-07-06 22:12:43 +0900

    add auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Amplify from 'aws-amplify';
+import { AmplifyAuthenticator, AmplifySignIn } from '@aws-amplify/ui-react';
+import awsconfig from './aws-exports';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+Amplify.configure(awsconfig);
 
-export default App;
+const Signin = () =>(
+  <AmplifyAuthenticator >
+      <AmplifySignIn slot="sign-in" hideSignUp></AmplifySignIn>
+  </AmplifyAuthenticator>
+)
+export default Signin;
