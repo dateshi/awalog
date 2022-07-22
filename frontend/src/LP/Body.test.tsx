3449b2171ee9d4640d4b7f62c21502fc984d8bf5
wotsushi <wotsushi@gmail.com>
2022-07-22 20:42:38 +0900

    リファクタリング

--- frontend/src/LP/Body.test.tsx
+++ frontend/src/LP/Body.test.tsx
@@ -25,8 +25,8 @@ describe("LP/Body", () => {
     it("初期状態のLPは8000である", () => {
       render(DefaultBody);
 
-      expect(screen.getByTestId("window-lp-1p").textContent).toEqual("8000");
-      expect(screen.getByTestId("window-lp-2p").textContent).toEqual("8000");
+      expect(screen.getByTestId("window-lp-1p")).toHaveTextContent("8000");
+      expect(screen.getByTestId("window-lp-2p")).toHaveTextContent("8000");
     });
     it("初期状態のログは空である", async () => {
       render(DefaultBody);
@@ -45,42 +45,42 @@ describe("LP/Body", () => {
       it("お互いのLPが8000の状態で1PのLPを-1000すると1PのLPが7000になる。2PのLPは8000のまま", async () => {
         render(DefaultBody);
 
-        expect(screen.getByTestId("window-lp-1p").textContent).toEqual("8000");
-        expect(screen.getByTestId("window-lp-2p").textContent).toEqual("8000");
+        expect(screen.getByTestId("window-lp-1p")).toHaveTextContent("8000");
+        expect(screen.getByTestId("window-lp-2p")).toHaveTextContent("8000");
 
         await user.click(screen.getAllByText("-1000")[0]);
 
-        expect(screen.getByTestId("window-lp-1p").textContent).toEqual("7000");
-        expect(screen.getByTestId("window-lp-2p").textContent).toEqual("8000");
+        expect(screen.getByTestId("window-lp-1p")).toHaveTextContent("7000");
+        expect(screen.getByTestId("window-lp-2p")).toHaveTextContent("8000");
       });
       it("お互いのLPが8000の状態で2PのLPを-2000すると1PのLPは8000のままだが2PのLPは6000になる", async () => {
         render(DefaultBody);
 
-        expect(screen.getByTestId("window-lp-1p").textContent).toEqual("8000");
-        expect(screen.getByTestId("window-lp-2p").textContent).toEqual("8000");
+        expect(screen.getByTestId("window-lp-1p")).toHaveTextContent("8000");
+        expect(screen.getByTestId("window-lp-2p")).toHaveTextContent("8000");
 
         await user.click(screen.getAllByText("-2000")[1]);
 
-        expect(screen.getByTestId("window-lp-1p").textContent).toEqual("8000");
-        expect(screen.getByTestId("window-lp-2p").textContent).toEqual("6000");
+        expect(screen.getByTestId("window-lp-1p")).toHaveTextContent("8000");
+        expect(screen.getByTestId("window-lp-2p")).toHaveTextContent("6000");
       });
       it("1PのLPが2000の状態で1PのLPを-3000すると1PのLPは0になる（マイナスにはならない）", async () => {
         render(DefaultBody);
         await user.click(screen.getAllByText("-3000")[0]);
         await user.click(screen.getAllByText("-3000")[0]);
 
-        expect(screen.getByTestId("window-lp-1p").textContent).toEqual("2000");
+        expect(screen.getByTestId("window-lp-1p")).toHaveTextContent("2000");
 
         await user.click(screen.getAllByText("-3000")[0]);
 
-        expect(screen.getByTestId("window-lp-1p").textContent).toEqual("0");
+        expect(screen.getByTestId("window-lp-1p")).toHaveTextContent("0");
       });
     });
     describe("LP加算", () => {
       it("お互いがnormalモードで1Pの+キーを押すと1P側はテンキー配置になる。2Pはそのまま", async () => {
         render(DefaultBody);
 
-        expect(screen.getAllByText("-50").length).toEqual(2);
+        expect(screen.getAllByText("-50")).toHaveLength(2);
         expect(screen.queryByText("7")).not.toBeInTheDocument();
 
         await user.click(screen.getAllByText("+")[0]);
@@ -91,7 +91,7 @@ describe("LP/Body", () => {
       it("LPが8000の状態で+1234を入力するとLP欄に8000+1234が表示される", async () => {
         render(DefaultBody);
 
-        expect(screen.getByTestId("window-lp-1p").textContent).toEqual("8000");
+        expect(screen.getByTestId("window-lp-1p")).toHaveTextContent("8000");
 
         await user.click(screen.getAllByText("+")[0]);
         await user.click(screen.getByText("1"));
@@ -99,16 +99,16 @@ describe("LP/Body", () => {
         await user.click(screen.getByText("3"));
         await user.click(screen.getByText("4"));
 
-        expect(screen.getByTestId("window-lp-1p").textContent).toEqual(
+        expect(screen.getByTestId("window-lp-1p")).toHaveTextContent(
           "8000+1234"
         );
-        expect(screen.getByTestId("window-lp-2p").textContent).toEqual("8000");
+        expect(screen.getByTestId("window-lp-2p")).toHaveTextContent("8000");
       });
       it("お互いのLPが8000の状態で1PのLPを+1234すると1PのLPは9234になる。2Pは8000のまま", async () => {
         render(DefaultBody);
 
-        expect(screen.getByTestId("window-lp-1p").textContent).toEqual("8000");
-        expect(screen.getByTestId("window-lp-2p").textContent).toEqual("8000");
+        expect(screen.getByTestId("window-lp-1p")).toHaveTextContent("8000");
+        expect(screen.getByTestId("window-lp-2p")).toHaveTextContent("8000");
 
         await user.click(screen.getAllByText("+")[0]);
         await user.click(screen.getByText("1"));
@@ -117,8 +117,8 @@ describe("LP/Body", () => {
         await user.click(screen.getByText("4"));
         await user.click(screen.getByText("="));
 
-        expect(screen.getByTestId("window-lp-1p").textContent).toEqual("9234");
-        expect(screen.getByTestId("window-lp-2p").textContent).toEqual("8000");
+        expect(screen.getByTestId("window-lp-1p")).toHaveTextContent("9234");
+        expect(screen.getByTestId("window-lp-2p")).toHaveTextContent("8000");
       });
       it("LP加算後はnormalモードに戻る", async () => {
         render(DefaultBody);
@@ -130,14 +130,14 @@ describe("LP/Body", () => {
         await user.click(screen.getByText("1"));
         await user.click(screen.getByText("="));
 
-        expect(screen.getAllByText("-50").length).toEqual(2);
+        expect(screen.getAllByText("-50")).toHaveLength(2);
         expect(screen.queryByText("7")).not.toBeInTheDocument();
       });
       it("お互いのLPが8000の状態で2PのLPを+9000すると1PのLPは8000のままだが2PのLPは17000になる", async () => {
         render(DefaultBody);
 
-        expect(screen.getByTestId("window-lp-1p").textContent).toEqual("8000");
-        expect(screen.getByTestId("window-lp-2p").textContent).toEqual("8000");
+        expect(screen.getByTestId("window-lp-1p")).toHaveTextContent("8000");
+        expect(screen.getByTestId("window-lp-2p")).toHaveTextContent("8000");
 
         await user.click(screen.getAllByText("+")[1]);
         await user.click(screen.getByText("9"));
@@ -145,14 +145,14 @@ describe("LP/Body", () => {
         await user.click(screen.getByText("00"));
         await user.click(screen.getByText("="));
 
-        expect(screen.getByTestId("window-lp-1p").textContent).toEqual("8000");
-        expect(screen.getByTestId("window-lp-2p").textContent).toEqual("17000");
+        expect(screen.getByTestId("window-lp-1p")).toHaveTextContent("8000");
+        expect(screen.getByTestId("window-lp-2p")).toHaveTextContent("17000");
       });
 
       it("LPが8000の状態で+100000すると1PのLPは108000になる（LPに上限はない）", async () => {
         render(DefaultBody);
 
-        expect(screen.getByTestId("window-lp-1p").textContent).toEqual("8000");
+        expect(screen.getByTestId("window-lp-1p")).toHaveTextContent("8000");
 
         await user.click(screen.getAllByText("+")[0]);
         await user.click(screen.getByText("1"));
@@ -161,16 +161,14 @@ describe("LP/Body", () => {
         await user.click(screen.getByText("0"));
         await user.click(screen.getByText("="));
 
-        expect(screen.getByTestId("window-lp-1p").textContent).toEqual(
-          "108000"
-        );
+        expect(screen.getByTestId("window-lp-1p")).toHaveTextContent("108000");
       });
     });
     describe("LP減算", () => {
       it("お互いがnormalモードで1Pの-キーを押すと1P側はテンキー配置になる。2Pはそのまま", async () => {
         render(DefaultBody);
 
-        expect(screen.getAllByText("-50").length).toEqual(2);
+        expect(screen.getAllByText("-50")).toHaveLength(2);
         expect(screen.queryByText("7")).not.toBeInTheDocument();
 
         await user.click(screen.getAllByText("+")[0]);
@@ -181,7 +179,7 @@ describe("LP/Body", () => {
       it("LPが8000の状態で-1234を入力するとLP欄に8000-1234が表示される", async () => {
         render(DefaultBody);
 
-        expect(screen.getByTestId("window-lp-1p").textContent).toEqual("8000");
+        expect(screen.getByTestId("window-lp-1p")).toHaveTextContent("8000");
 
         await user.click(screen.getAllByText("-")[0]);
         await user.click(screen.getByText("1"));
@@ -189,16 +187,16 @@ describe("LP/Body", () => {
         await user.click(screen.getByText("3"));
         await user.click(screen.getByText("4"));
 
-        expect(screen.getByTestId("window-lp-1p").textContent).toEqual(
+        expect(screen.getByTestId("window-lp-1p")).toHaveTextContent(
           "8000-1234"
         );
-        expect(screen.getByTestId("window-lp-2p").textContent).toEqual("8000");
+        expect(screen.getByTestId("window-lp-2p")).toHaveTextContent("8000");
       });
       it("お互いのLPが8000の状態で1PのLPを-1234すると1PのLPは6766になる。2Pは8000のまま", async () => {
         render(DefaultBody);
 
-        expect(screen.getByTestId("window-lp-1p").textContent).toEqual("8000");
-        expect(screen.getByTestId("window-lp-2p").textContent).toEqual("8000");
+        expect(screen.getByTestId("window-lp-1p")).toHaveTextContent("8000");
+        expect(screen.getByTestId("window-lp-2p")).toHaveTextContent("8000");
 
         await user.click(screen.getAllByText("-")[0]);
         await user.click(screen.getByText("1"));
@@ -207,8 +205,8 @@ describe("LP/Body", () => {
         await user.click(screen.getByText("4"));
         await user.click(screen.getByText("="));
 
-        expect(screen.getByTestId("window-lp-1p").textContent).toEqual("6766");
-        expect(screen.getByTestId("window-lp-2p").textContent).toEqual("8000");
+        expect(screen.getByTestId("window-lp-1p")).toHaveTextContent("6766");
+        expect(screen.getByTestId("window-lp-2p")).toHaveTextContent("8000");
       });
       it("LP減算後はnormalモードに戻る", async () => {
         render(DefaultBody);
@@ -220,21 +218,21 @@ describe("LP/Body", () => {
         await user.click(screen.getByText("1"));
         await user.click(screen.getByText("="));
 
-        expect(screen.getAllByText("-50").length).toEqual(2);
+        expect(screen.getAllByText("-50")).toHaveLength(2);
         expect(screen.queryByText("7")).not.toBeInTheDocument();
       });
       it("お互いのLPが8000の状態で2PのLPを-1すると1PのLPは8000のままだが2PのLPは7999になる", async () => {
         render(DefaultBody);
 
-        expect(screen.getByTestId("window-lp-1p").textContent).toEqual("8000");
-        expect(screen.getByTestId("window-lp-2p").textContent).toEqual("8000");
+        expect(screen.getByTestId("window-lp-1p")).toHaveTextContent("8000");
+        expect(screen.getByTestId("window-lp-2p")).toHaveTextContent("8000");
 
         await user.click(screen.getAllByText("-")[1]);
         await user.click(screen.getByText("1"));
         await user.click(screen.getByText("="));
 
-        expect(screen.getByTestId("window-lp-1p").textContent).toEqual("8000");
-        expect(screen.getByTestId("window-lp-2p").textContent).toEqual("7999");
+        expect(screen.getByTestId("window-lp-1p")).toHaveTextContent("8000");
+        expect(screen.getByTestId("window-lp-2p")).toHaveTextContent("7999");
       });
       it("LPが1233の状態でLPを-1234するとLPは0になる（マイナスにはならない）", async () => {
         render(DefaultBody);
@@ -245,7 +243,7 @@ describe("LP/Body", () => {
         await user.click(screen.getByText("7"));
         await user.click(screen.getByText("="));
 
-        expect(screen.getByTestId("window-lp-1p").textContent).toEqual("1233");
+        expect(screen.getByTestId("window-lp-1p")).toHaveTextContent("1233");
 
         await user.click(screen.getAllByText("-")[0]);
         await user.click(screen.getByText("1"));
@@ -254,7 +252,7 @@ describe("LP/Body", () => {
         await user.click(screen.getByText("4"));
         await user.click(screen.getByText("="));
 
-        expect(screen.getByTestId("window-lp-1p").textContent).toEqual("0");
+        expect(screen.getByTestId("window-lp-1p")).toHaveTextContent("0");
       });
     });
     describe("加減算モードのキャンセル", () => {
@@ -263,29 +261,27 @@ describe("LP/Body", () => {
         await user.click(screen.getAllByText("+")[0]);
 
         expect(screen.getByText("7")).toBeInTheDocument();
-        expect(screen.getAllByText("-50").length).toEqual(1);
+        expect(screen.getByText("-50")).toBeInTheDocument();
 
         await user.click(screen.getByText("C"));
 
         expect(screen.queryByText("7")).not.toBeInTheDocument();
-        expect(screen.getAllByText("-50").length).toEqual(2);
+        expect(screen.getAllByText("-50")).toHaveLength(2);
       });
       it("減算モードでLP入力中にキャンセルするとnormalモードになり入力していたLPもクリアされる", async () => {
         render(DefaultBody);
         await user.click(screen.getAllByText("-")[1]);
         await user.click(screen.getByText("1"));
 
-        expect(screen.getByTestId("window-lp-2p").textContent).toEqual(
-          "8000-1"
-        );
+        expect(screen.getByTestId("window-lp-2p")).toHaveTextContent("8000-1");
         expect(screen.getByText("7")).toBeInTheDocument();
-        expect(screen.getAllByText("-50").length).toEqual(1);
+        expect(screen.getAllByText("-50")).toHaveLength(1);
 
         await user.click(screen.getByText("C"));
 
-        expect(screen.getByTestId("window-lp-2p").textContent).toEqual("8000");
+        expect(screen.getByTestId("window-lp-2p")).toHaveTextContent("8000");
         expect(screen.queryByText("7")).not.toBeInTheDocument();
-        expect(screen.getAllByText("-50").length).toEqual(2);
+        expect(screen.getAllByText("-50")).toHaveLength(2);
       });
       it("1Pが加算モード、2Pが減算モードの状態で2P側をキャンセルすると1Pは加算モードのままだが2Pはnormalモードになる", async () => {
         render(DefaultBody);
@@ -294,21 +290,15 @@ describe("LP/Body", () => {
         await user.click(screen.getAllByText("-")[1]);
         await user.click(screen.getAllByText("2")[1]);
 
-        expect(screen.getByTestId("window-lp-1p").textContent).toEqual(
-          "8000+1"
-        );
-        expect(screen.getByTestId("window-lp-2p").textContent).toEqual(
-          "8000-2"
-        );
+        expect(screen.getByTestId("window-lp-1p")).toHaveTextContent("8000+1");
+        expect(screen.getByTestId("window-lp-2p")).toHaveTextContent("8000-2");
         expect(screen.queryByText("-50")).not.toBeInTheDocument();
-        expect(screen.getAllByText("7").length).toEqual(2);
+        expect(screen.getAllByText("7")).toHaveLength(2);
 
         await user.click(screen.getAllByText("C")[1]);
 
-        expect(screen.getByTestId("window-lp-1p").textContent).toEqual(
-          "8000+1"
-        );
-        expect(screen.getByTestId("window-lp-2p").textContent).toEqual("8000");
+        expect(screen.getByTestId("window-lp-1p")).toHaveTextContent("8000+1");
+        expect(screen.getByTestId("window-lp-2p")).toHaveTextContent("8000");
         expect(screen.getByText("7")).toBeInTheDocument();
         expect(screen.getByText("-50")).toBeInTheDocument();
       });
@@ -317,38 +307,36 @@ describe("LP/Body", () => {
         await user.click(screen.getAllByText("+")[0]);
         await user.click(screen.getByText("1"));
 
-        expect(screen.getByTestId("window-lp-1p").textContent).toEqual(
-          "8000+1"
-        );
+        expect(screen.getByTestId("window-lp-1p")).toHaveTextContent("8000+1");
 
         await user.click(screen.getByText("C"));
         await user.click(screen.getAllByText("+")[0]);
 
-        expect(screen.getByTestId("window-lp-1p").textContent).toEqual("8000");
+        expect(screen.getByTestId("window-lp-1p")).toHaveTextContent("8000");
       });
     });
     describe("LP半分", () => {
       it("お互いのLPが8000の状態で1PのLPを半分にすると1PのLPは4000になる。2PのLPは8000のまま", async () => {
         render(DefaultBody);
 
-        expect(screen.getByTestId("window-lp-1p").textContent).toEqual("8000");
-        expect(screen.getByTestId("window-lp-2p").textContent).toEqual("8000");
+        expect(screen.getByTestId("window-lp-1p")).toHaveTextContent("8000");
+        expect(screen.getByTestId("window-lp-2p")).toHaveTextContent("8000");
 
         await user.click(screen.getAllByText("1/2")[0]);
 
-        expect(screen.getByTestId("window-lp-1p").textContent).toEqual("4000");
-        expect(screen.getByTestId("window-lp-2p").textContent).toEqual("8000");
+        expect(screen.getByTestId("window-lp-1p")).toHaveTextContent("4000");
+        expect(screen.getByTestId("window-lp-2p")).toHaveTextContent("8000");
       });
       it("お互いのLPが8000の状態で2PのLPを半分にすると1PのLPは8000のままだが2PのLPは4000になる", async () => {
         render(DefaultBody);
 
-        expect(screen.getByTestId("window-lp-1p").textContent).toEqual("8000");
-        expect(screen.getByTestId("window-lp-2p").textContent).toEqual("8000");
+        expect(screen.getByTestId("window-lp-1p")).toHaveTextContent("8000");
+        expect(screen.getByTestId("window-lp-2p")).toHaveTextContent("8000");
 
         await user.click(screen.getAllByText("1/2")[1]);
 
-        expect(screen.getByTestId("window-lp-1p").textContent).toEqual("8000");
-        expect(screen.getByTestId("window-lp-2p").textContent).toEqual("4000");
+        expect(screen.getByTestId("window-lp-1p")).toHaveTextContent("8000");
+        expect(screen.getByTestId("window-lp-2p")).toHaveTextContent("4000");
       });
       it("LPが奇数の状態でLPを半分にするとLPは切り上げられる", async () => {
         render(DefaultBody);
@@ -356,11 +344,11 @@ describe("LP/Body", () => {
         await user.click(screen.getByText("1"));
         await user.click(screen.getByText("="));
 
-        expect(screen.getByTestId("window-lp-1p").textContent).toEqual("7999");
+        expect(screen.getByTestId("window-lp-1p")).toHaveTextContent("7999");
 
         await user.click(screen.getAllByText("1/2")[0]);
 
-        expect(screen.getByTestId("window-lp-1p").textContent).toEqual("4000");
+        expect(screen.getByTestId("window-lp-1p")).toHaveTextContent("4000");
       });
       it("LPが1の状態でLPを半分にしてもLPは1のまま", async () => {
         render(DefaultBody);
@@ -371,11 +359,11 @@ describe("LP/Body", () => {
         await user.click(screen.getByText("9"));
         await user.click(screen.getByText("="));
 
-        expect(screen.getByTestId("window-lp-1p").textContent).toEqual("1");
+        expect(screen.getByTestId("window-lp-1p")).toHaveTextContent("1");
 
         await user.click(screen.getAllByText("1/2")[0]);
 
-        expect(screen.getByTestId("window-lp-1p").textContent).toEqual("1");
+        expect(screen.getByTestId("window-lp-1p")).toHaveTextContent("1");
       });
     });
   });
@@ -485,7 +473,7 @@ describe("LP/Body", () => {
       await user.click(screen.getAllByText("-1000")[0]);
       await user.click(screen.getByText("ログ"));
 
-      expect(screen.getAllByTestId("modal-log").length).toEqual(10);
+      expect(screen.getAllByTestId("modal-log")).toHaveLength(10);
       expect(screen.getAllByTestId("modal-log")[9]).toHaveTextContent(
         "旋風BF (1P)"
       );
@@ -497,7 +485,7 @@ describe("LP/Body", () => {
       await user.click(screen.getAllByText("-2000")[0]);
       await user.click(screen.getByText("ログ"));
 
-      expect(screen.getAllByTestId("modal-log").length).toEqual(10);
+      expect(screen.getAllByTestId("modal-log")).toHaveLength(10);
       expect(screen.getAllByTestId("modal-log")[9]).toHaveTextContent(
         "旋風BF (1P)"
       );
