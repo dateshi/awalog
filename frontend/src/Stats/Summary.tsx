2dd76ef1782760d77ec0e0a90f64b90167bf4642
wotsushi <wotsushi@gmail.com>
2022-07-31 10:45:57 +0900

    各デッキの勝率: タイトルと軸ラベル設定

--- frontend/src/Stats/Summary.tsx
+++ frontend/src/Stats/Summary.tsx
@@ -22,18 +22,30 @@ const Summary = () => {
   return (
     <Bar
       options={{
+        scales: {
+          y: {
+            title: {
+              display: true,
+              text: "勝率[%]",
+            },
+          },
+        },
         plugins: {
+          title: {
+            display: true,
+            text: "各デッキの勝率",
+          },
           legend: {
             display: false,
           },
         },
       }}
       data={{
-        labels: ["旋風BF", "代行天使"],
+        labels: ["旋風BF", "代行天使", "ヒーロービート", "六武衆"],
         datasets: [
           {
             label: "勝率",
-            data: [45, 55],
+            data: [45, 55, 50, 52.5],
             backgroundColor: "rgba(53, 162, 235, 0.5)",
           },
         ],
