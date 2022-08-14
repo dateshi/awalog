e1cbb010701454afced96a77b606df045e35d01e
wotsushi <wotsushi@gmail.com>
2022-08-14 10:41:31 +0900

    戦績ページを中央揃え

--- frontend/src/Stats/Body.tsx
+++ frontend/src/Stats/Body.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Container } from "react-bootstrap";
 import { Result } from "../result";
 import Detail from "./Detail";
 import Sidebar from "./Sidebar";
@@ -13,14 +14,16 @@ const Body = (props: Props) => {
   const { results, decks } = props;
   const [deck, setDeck] = useState("サマリー");
   return (
-    <div className="body">
-      <Sidebar decks={decks} selectedDeck={deck} setDeck={setDeck} />
-      {deck === "サマリー" ? (
-        <Summary results={results} />
-      ) : (
-        <Detail results={results} deck={deck} />
-      )}
-    </div>
+    <Container>
+      <div className="body">
+        <Sidebar decks={decks} selectedDeck={deck} setDeck={setDeck} />
+        {deck === "サマリー" ? (
+          <Summary results={results} />
+        ) : (
+          <Detail results={results} deck={deck} />
+        )}
+      </div>
+    </Container>
   );
 };
 
